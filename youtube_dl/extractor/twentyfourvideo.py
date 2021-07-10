443533da5ba37cc6429cb4537c77ce32cc21ff7b
df <fieldhouse@gmx.net>
2021-07-10 16:39:24 +0100

    Support more 24video domains

--- youtube_dl/extractor/twentyfourvideo.py
+++ youtube_dl/extractor/twentyfourvideo.py
@@ -11,21 +11,21 @@ from ..utils import (
     xpath_element,
 )
 
-
 class TwentyFourVideoIE(InfoExtractor):
     IE_NAME = '24video'
     _VALID_URL = r'''(?x)
                     https?://
                         (?P<host>
-                            (?:(?:www|porno?)\.)?24video\.
-                            (?:net|me|xxx|sexy?|tube|adult|site|vip)
+                            (?:(?:www|porno?|sexy?|m)\.)?24video\.
+                            (?:net|me|xxx|sexy?|tube|adult|site|vip|in)
                         )/
                         (?:
                             video/(?:(?:view|xml)/)?|
                             player/new24_play\.swf\?id=
                         )
-                        (?P<id>\d+)
+                        (?P<id>\d+)(?:-[^/#??]+)?
                     '''
+    _GEO_COUNTRIES = ['PK']
 
     _TESTS = [{
         'url': 'http://www.24video.net/video/view/1044982',
@@ -76,9 +76,10 @@ class TwentyFourVideoIE(InfoExtractor):
             'http://%s/video/view/%s' % (host, video_id), video_id)
 
         title = self._og_search_title(webpage)
-        description = self._html_search_regex(
+        description = (self._html_search_regex(
             r'<(p|span)[^>]+itemprop="description"[^>]*>(?P<description>[^<]+)</\1>',
-            webpage, 'description', fatal=False, group='description')
+            webpage, 'description', default=None, group='description')
+            or self._og_search_description(webpage))
         thumbnail = self._og_search_thumbnail(webpage)
         duration = int_or_none(self._og_search_property(
             'duration', webpage, 'duration', fatal=False))
@@ -86,36 +87,50 @@ class TwentyFourVideoIE(InfoExtractor):
             r'<time[^>]+\bdatetime="([^"]+)"[^>]+itemprop="uploadDate"',
             webpage, 'upload date', fatal=False))
 
-        uploader = self._html_search_regex(
-            r'class="video-uploaded"[^>]*>\s*<a href="/jsecUser/movies/[^"]+"[^>]*>([^<]+)</a>',
-            webpage, 'uploader', fatal=False)
+        url = self._html_search_meta('ya:ovs:content_url', webpage, default=None)
+        if url:
+            formats = [{ 'url': url }]
+            uploader = None
+            view_count = int_or_none(self._html_search_meta('ya:ovs:views_total', webpage, default=None))
+            comment_count = int_or_none(self._html_search_meta('ya:ovs:comments', webpage, default=None))
+            like_count = int_or_none(self._html_search_meta('ya:ovs:likes', webpage, default=None))
+            like_count = int_or_none(self._html_search_meta('ya:ovs:likes', webpage, default=None))
+            dislike_count = int_or_none(self._html_search_meta('ya:ovs:dislikes', webpage, default=None))
+            age_limit = self._family_friendly_search(webpage) 
+            if age_limit is None:
+                age_limit = 18
+        else:
+            uploader = self._html_search_regex(
+                r'class="video-uploaded"[^>]*>\s*<a href="/jsecUser/movies/[^"]+"[^>]*>([^<]+)</a>',
+                webpage, 'uploader', fatal=False)
+            view_count = int_or_none(self._html_search_regex(
+                r'<span class="video-views">(\d+) просмотр',
+                webpage, 'view count', fatal=False))
+            comment_count = int_or_none(self._html_search_regex(
+                r'<a[^>]+href="#tab-comments"[^>]*>(\d+) комментари',
+                webpage, 'comment count', default=None))
 
-        view_count = int_or_none(self._html_search_regex(
-            r'<span class="video-views">(\d+) просмотр',
-            webpage, 'view count', fatal=False))
-        comment_count = int_or_none(self._html_search_regex(
-            r'<a[^>]+href="#tab-comments"[^>]*>(\d+) комментари',
-            webpage, 'comment count', default=None))
+            # Sets some cookies
+            self._download_xml(
+                r'http://%s/video/xml/%s?mode=init' % (host, video_id),
+                video_id, 'Downloading init XML')
 
-        # Sets some cookies
-        self._download_xml(
-            r'http://%s/video/xml/%s?mode=init' % (host, video_id),
-            video_id, 'Downloading init XML')
+            video_xml = self._download_xml(
+                'http://%s/video/xml/%s?mode=play' % (host, video_id),
+                video_id, 'Downloading video XML')
 
-        video_xml = self._download_xml(
-            'http://%s/video/xml/%s?mode=play' % (host, video_id),
-            video_id, 'Downloading video XML')
+            video = xpath_element(video_xml, './/video', 'video', fatal=True)
 
-        video = xpath_element(video_xml, './/video', 'video', fatal=True)
+            formats = [{
+                'url': xpath_attr(video, '', 'url', 'video URL', fatal=True),
+            }]
 
-        formats = [{
-            'url': xpath_attr(video, '', 'url', 'video URL', fatal=True),
-        }]
+            like_count = int_or_none(video.get('ratingPlus'))
+            dislike_count = int_or_none(video.get('ratingMinus'))
+            age_limit = 18 if video.get('adult') == 'true' else 0
 
-        like_count = int_or_none(video.get('ratingPlus'))
-        dislike_count = int_or_none(video.get('ratingMinus'))
-        age_limit = 18 if video.get('adult') == 'true' else 0
-
+        self._sort_formats(formats)
+        
         return {
             'id': video_id,
             'title': title,
